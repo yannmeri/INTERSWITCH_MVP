dddfdaad16cdbe18dec9302f29ebfd102bd606b0
DevGBSS <david.gbessemehlan@epitech.eu>
2025-01-28 20:14:13 +0100

    add file who make the control fir user routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,16 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
-// Fonction pour enregistrer un utilisateur
 exports.registerUser = async (req, res) => {
   const { name, email, password, role } = req.body;
 
   try {
-    // Vérifier si l'email existe déjà
     const userExists = await User.findOne({ where: { email } });
     if (userExists) {
       return res.status(400).json({ message: "Cet email est déjà utilisé." });
     }
 
-    // Hasher le mot de passe
     const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Créer l'utilisateur
     const newUser = await User.create({
       name,
       email,
