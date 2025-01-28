12df2abaeb314e48ad9f1af641d65d231790c2cb
DevGBSS <david.gbessemehlan@epitech.eu>
2025-01-28 20:14:45 +0100

    do the register routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { registerUser } = require("../controllers/userController");
+const paymentService = require('../services/paymentService');
 const router = express.Router();
 
 router.post("/register", registerUser);
